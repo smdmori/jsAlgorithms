675cab1f2a9918407deb73a7911ac227766b5c1c
smdmori <mortezasamadi1371@gmail.com>
2021-09-13 12:10:06 +0430

    Create binary search tree constructor

---
+++ bst/bst-01-constructor.js
@@ -0,0 +1,16 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.right = null
+        this.left = null
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null
+    }
+}
+
+let bst = new BST()
+bst
