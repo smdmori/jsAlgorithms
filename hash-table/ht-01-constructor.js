b21a8f635440b317e082805fe84d2d6201619277
smdmori <mortezasamadi1371@gmail.com>
2021-09-14 21:38:42 +0430

    refactor constructor

--- hash-table/ht-01-constructor.js
+++ hash-table/ht-01-constructor.js
@@ -6,8 +6,7 @@ class HashTable {
     _hash(key) {
         let hash = 0
         for ( let i = 0; i < key.length; i++ ) {
-            hash = ( hash + i.charCodeAt(i) * 23 ) % this.dataMap.length
-            console.log('i >> ' + i + ' : hash >> ' + hash)
+            hash = ( hash + key.charCodeAt(i) * 23 ) % this.dataMap.length
         }
         return hash
     }
